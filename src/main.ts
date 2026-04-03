5bf852a40a6a65d16504afb363ae66f8e990c9ba
compumike08 <compumike08@users.noreply.github.com>
2026-04-03 01:14:24 -0400

    Update main electron window size

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,8 @@ if (started) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 750,
-    height: 1000,
+    width: 1250,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, "preload.js")
     },
